de44b366b49d956bea57f56d1c8f4d024ebacd28
gieong <gabrielieong99@hotmail.com>
2021-02-26 01:10:21 -0800

    refined game with more options and compounding multipliers

--- HighLow.hs
+++ HighLow.hs
@@ -3,6 +3,7 @@ import System.Random
 import Data.Array.IO
 import Control.Monad
 import System.IO.Unsafe 
+import Funds
 {- If the random import gives you issues when compiling, try (on a new terminal):
         stack import random
         stack ghci
@@ -57,23 +58,27 @@ suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
 randomSuit gen = " of " ++ show (suits !! fst (randomR (0, 3) gen))
 -}
 
--- end of game: value (0 = tie, 1 = win, 2 = lose), starting next state
-data Result = EndOfGame Int State
+-- end of game: value (0 = tie, 1 = win, 2 = lose), starting next state, prior winnings
+data Result = EndOfGame Int State Double
     deriving (Eq, Show)
 
--- a state is a state where currentCard nextCard savedChoice playerfunds bet [remaining cards in deck]
-data State = State Int Int String Double Double [Int]
+-- a state is a state where currentCard nextCard savedChoice playerfunds bet [remaining cards in deck] multiplier winnings
+data State = State Int Int String Double Double [Int] Double Double
     deriving (Ord, Eq, Show)
 
 -- a highlow game takes a state (current hand), returns result tie, win or lose
 type Game = State -> State
 
 highlow :: State -> String -> Result
-highlow (State currentCard nextCard savedChoice playerfunds bet currDeck) choice
-    | tie currentCard nextCard choice = EndOfGame 0 (State currentCard nextCard choice (playerfunds + bet) 0 currDeck)
-    | lose currentCard nextCard choice = EndOfGame 1 (State currentCard nextCard choice playerfunds 0 currDeck)
-    | win currentCard nextCard choice = EndOfGame 2 (State currentCard nextCard choice (playerfunds + (m currentCard choice)*bet) bet currDeck)
-    | otherwise = EndOfGame 1 (State currentCard nextCard choice playerfunds 0 currDeck)
+highlow (State currentCard nextCard savedChoice playerfunds bet currDeck mult winnings) choice
+    | tie currentCard nextCard choice = 
+        let newMult = mult * 1.1
+        in EndOfGame 0 (State currentCard nextCard choice playerfunds bet currDeck newMult (newMult * (winnings + bet))) winnings
+    | lose currentCard nextCard choice = EndOfGame 1 (State currentCard nextCard choice playerfunds bet currDeck 1 0) winnings
+    | win currentCard nextCard choice = 
+        let newMult = (mult * (m currentCard choice) )
+        in EndOfGame 2 (State currentCard nextCard choice playerfunds bet currDeck newMult ( newMult * (winnings + bet))) winnings
+    | otherwise = EndOfGame 1 (State currentCard nextCard choice (playerfunds + winnings) bet currDeck mult winnings) winnings
 
 -- tie if currentCard and nextCard are equal
 tie currentCard nextCard choice = 
@@ -130,18 +135,18 @@ m prevCard playerGuess
 
 -- draw card to keep track of current card and perhaps nextCard??
 drawCard :: State -> IO State
-drawCard (State currentCard nextCard savedChoice playerFunds bet []) = drawCard (State currentCard nextCard savedChoice playerFunds bet (createDeck))
+drawCard (State currentCard nextCard savedChoice playerFunds bet [] mult winnings) = drawCard (State currentCard nextCard savedChoice playerFunds bet (createDeck) mult winnings)
 
-drawCard (State currentCard nextCard savedChoice playerFunds bet (f:r)) 
-    | nextCard == 0 = drawCard (State currentCard f savedChoice playerFunds bet r)
-    | otherwise = return (State nextCard f savedChoice playerFunds bet r)
+drawCard (State currentCard nextCard savedChoice playerFunds bet (f:r) mult winnings) 
+    | nextCard == 0 = drawCard (State currentCard f savedChoice playerFunds bet r mult winnings)
+    | otherwise = return (State nextCard f savedChoice playerFunds bet r mult winnings)
 
 createDeck = unsafePerformIO (shuffle decks)
 
 -- playersTurn
 playersTurn :: State -> IO State
 playersTurn p = do
-    putStrLn $ "Your Card: " ++ show (toCard (getCurrentCard p)) ++ "\n"
+    putStrLn $ "Your Card: " ++ show (toCard (getCurrentCard p)) ++ ""
     putStrLn "High or Low?"
     ans <- getLineCorr
     if ans `elem` ["high", "High"] 
@@ -159,8 +164,8 @@ playersTurn p = do
 
 -- gets the users bet
 placeBet :: State -> IO State
-placeBet (State currentCard nextCard savedChoice playerFunds bet deck) = do
-    putStrLn $ "You have $" ++ show playerFunds ++ "\n"
+placeBet (State currentCard nextCard savedChoice playerFunds bet deck mult winnings) = do
+    putStrLn $ "You have $" ++ strRound(show playerFunds) ++ ""
     putStrLn "How much would you like to bet?"
     b <- getLineCorr
     if isDouble b 
@@ -169,16 +174,17 @@ placeBet (State currentCard nextCard savedChoice playerFunds bet deck) = do
             if playerBet <= 0
                     then do 
                     putStrLn "You have to bet more than $0." 
-                    placeBet (State currentCard nextCard savedChoice playerFunds 0 deck)
+                    placeBet (State currentCard nextCard savedChoice playerFunds 0 deck mult winnings)
             else if playerBet <= playerFunds 
                 then do
-                    return (State currentCard nextCard savedChoice (playerFunds - playerBet) playerBet deck) 
+                    putStrLn $ "You now have $" ++ strRound(show (playerFunds - playerBet)) ++ "\n"
+                    return (State currentCard nextCard savedChoice (playerFunds - playerBet) playerBet deck mult winnings) 
                 else do
                     putStrLn "You don't have enough money! Please try again." 
-                    placeBet (State currentCard nextCard savedChoice playerFunds 0 deck)
+                    placeBet (State currentCard nextCard savedChoice playerFunds 0 deck mult winnings)
         else do
             putStrLn "Invalid input. Please try again."
-            placeBet (State currentCard nextCard savedChoice playerFunds 0 deck)
+            placeBet (State currentCard nextCard savedChoice playerFunds 0 deck mult winnings)
 
 -- determines if a string is a double
 -- taken from https://rosettacode.org/wiki/Determine_if_a_string_is_numeric#Haskell
@@ -210,67 +216,104 @@ main = do
     play initState
 
 --initial State with $1000 (arbitrary default amount of money)
-initState = State 0 0 "" 1000 0 []
+initState = State 0 0 "" 1000 0 [] 1 0
 
 play :: State -> IO State
 play s = do
-    putStrLn "Would you like to play HighLow?"
+    putStrLn "Would you like to:"
+    putStrLn "- Play HighLow? (p/play)"
+    putStrLn "- Cash In?      (c/cash)"
+    putStrLn "- View Funds?   (v/view)"
+    putStrLn "- Quit App?     (q/quit)"
     ans <- getLineCorr
-    if ans `elem` ["y", "yes"] 
+    putStrLn ""
+    if ans `elem` ["p", "play"] 
         then do 
             a <- drawCard s
             b <- placeBet a
             playersTurn b
-    else if ans `elem` ["n","no"]
+    else if ans `elem` ["c","cash"]
         then do 
-            putStrLn "Thank you for visiting."
+            putStrLn "Use Display method"
+            putStrLn ""
+            play (State 0 0 "" ((getFunds s) + (getWinnings s)) 0 [] 1 0)
+    else if ans `elem` ["v", "view"]
+        then do
+            putStrLn "use display method"
+            putStrLn ""
+            play s
+    else if ans `elem` ["q","quit"]
+        then do 
+            putStrLn $ "Thank you for visiting. \n"
             return s
         else do
             putStrLn "Invalid input. Please try again."
-            putStrLn "Enter 'y' or 'yes' for YES and 'n' or 'no' for NO."
+            putStrLn ""
             play s
 
 -- show player's card
-getCurrentCard (State currentCard nextCard savedChoice playerfunds bet currDeck) = currentCard
+getCurrentCard (State currentCard nextCard savedChoice playerfunds bet currDeck mult winnings) = currentCard
 
 -- show next card
-getNextCard (State currentCard nextCard savedChoice playerfunds bet currDeck) = nextCard
+getNextCard (State currentCard nextCard savedChoice playerfunds bet currDeck mult winnings) = nextCard
 
 -- show choice
-getChoice (State currentCard nextCard savedChoice playerfunds bet currDeck) = savedChoice
+getChoice (State currentCard nextCard savedChoice playerfunds bet currDeck mult winnings) = savedChoice
 
 -- show bet
-getBet (State currentCard nextCard savedChoice playerfunds bet currDeck) = bet 
+getBet (State currentCard nextCard savedChoice playerfunds bet currDeck mult winnings) = bet 
 
 -- show deck
-getDeck (State currentCard nextCard savedChoice playerfunds bet currDeck) = currDeck
+getDeck (State currentCard nextCard savedChoice playerfunds bet currDeck mult winnings) = currDeck
+
+-- show multiplier 
+getMult (State currentCard nextCard savedChoice playerfunds bet currDeck mult winnings) = mult
+
+-- show winnings
+getWinnings (State currentCard nextCard savedChoice playerfunds bet currDeck mult winnings) = winnings
 
 -- show playerfunds
 getFunds :: State -> Double
-getFunds (State currentCard nextCard savedChoice playerfunds bet currDeck) = playerfunds
+getFunds (State currentCard nextCard savedChoice playerfunds bet currDeck mult winnings) = playerfunds
+
+-- round string to 2 decimals
+strRound ('.':a:b:c) = '.':[a]++[b]
+strRound [] = []
+strRound (h:t) = h:(strRound t)
 
 result :: Result -> IO State
-result (EndOfGame 0 s) = do
+result (EndOfGame 0 s w) = do
     putStrLn ""
     putStrLn $ "Your Card: " ++ show (toCard (getCurrentCard s)) ++ "\n"
     putStrLn $ "Next Card: " ++ show (toCard (getNextCard s)) ++ "\n"
     putStrLn $ "Your Choice: " ++ show (getChoice s) ++ "\n"
     putStrLn $ "-------------TIE-------------" ++ "\n"
-    putStrLn $ "Player Funds: " ++ show (getFunds s) ++ "\n"
+    putStrLn $ "Player Funds: " ++ strRound(show (getFunds s)) ++ "\n"
+    putStrLn $ "Winnings Summary:"
+    putStrLn $ "    = Multiplier x (PastWinnings + Bet)"
+    putStrLn $ "    = " ++ strRound (show (getMult s)) ++ " x ("  ++ strRound(show (w)) ++ " + "  ++ strRound(show (getBet s)) ++ ")"
+    putStrLn $ "    = $" ++ strRound (show (getWinnings s)) ++ "\n"
     return s
-result (EndOfGame 1 s) = do
+result (EndOfGame 1 s w) = do
     putStrLn ""
     putStrLn $ "Your Card: " ++ show (toCard (getCurrentCard s)) ++ "\n"
     putStrLn $ "Next Card: " ++ show (toCard (getNextCard s)) ++ "\n"
     putStrLn $ "Your Choice: " ++ show (getChoice s) ++ "\n"
     putStrLn $ "-------------LOSE-------------" ++ "\n"
-    putStrLn $ "Player Funds: " ++ show (getFunds s) ++ "\n"
+    putStrLn $ "Player Funds: " ++ strRound(show (getFunds s)) ++ ""
+    putStrLn $ "You LOST:"
+    putStrLn $ "- Your Bet: $" ++ strRound(show (getBet s)) ++ ""
+    putStrLn $ "- Your Winnings: $" ++ strRound(show (w)) ++ "\n"
     return s
-result (EndOfGame 2 s) = do
+result (EndOfGame 2 s w) = do
     putStrLn ""
     putStrLn $ "Your Card: " ++ show (toCard (getCurrentCard s)) ++ "\n"
     putStrLn $ "Next Card: " ++ show (toCard (getNextCard s)) ++ "\n"
     putStrLn $ "Your Choice: " ++ show (getChoice s) ++ "\n"
     putStrLn $ "-------------WIN-------------" ++ "\n"
-    putStrLn $ "Player Funds: " ++ show (getFunds s) ++ "\n"
+    putStrLn $ "Player Funds: " ++ strRound(show (getFunds s)) ++ "\n"
+    putStrLn $ "Winnings Summary:"
+    putStrLn $ "    = Multiplier x (PastWinnings + Bet)"
+    putStrLn $ "    = " ++ strRound (show (getMult s)) ++ " x ("  ++ strRound(show (w)) ++ " + "  ++ strRound(show (getBet s)) ++ ")"
+    putStrLn $ "    = $" ++ strRound (show (getWinnings s)) ++ "\n"
     return s
